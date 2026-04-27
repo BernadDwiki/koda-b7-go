2df3c8fd5ddb7d91a46db7399e9c55647f11afcb
Bernadus Dwiki Adicitra <dwiki.adicitra6@gmail.com>
2026-04-27 19:03:59 +0700

    refactor: clean code for minitask

--- main.go
+++ main.go
@@ -9,34 +9,6 @@ import (
 )
 
 func main() {
-	// var greeting string = "Koda"
-	// var a float32
-	// var b int
-
-	// d := true
-	// isSame := greeting == strconv.Itoa(b)
-
-	// fmt.Println(greeting)
-	// fmt.Printf("%s\n", greeting)
-	// fmt.Printf("%.2f\n", a)
-	// fmt.Printf("%t\n", d)
-	// fmt.Printf("%t\n", isSame)
-	// fmt.Println("Hello World!")
-	// hello()
-	// greet("dwiki")
-	// fmt.Println(multiplication(4, 2))
-	// fmt.Println(printToN(5))
-
-	// var ages [5]int = [5]int{15, 15, 16, 17, 20}
-	// fmt.Println(ages)
-
-	// num := []int{50, 75, 66, 20, 32, 90}
-	// sisipAngka(88, num)
-	// num := make([]int, 0, 8)
-	// num = append(num, 50, 75, 66, 20, 32, 90)
-	// num = sisipAngka(88, num)
-	// fmt.Println(num)
-
 	fmt.Printf("Luas = %.2f\n", minitask1.HitungLuas(5))
 	fmt.Printf("Keliling = %.2f\n", minitask1.HitungKeliling(5))
 	luas, keliling := minitask1.LuasKeliling(5)
@@ -68,33 +40,6 @@ func main() {
 	fmt.Println(dwiki.education[0].major)
 
 }
-
-// func hello() {
-// 	fmt.Println("hello")
-// }
-
-// func greet(name string) {
-// 	fmt.Printf("hello %s\n", name)
-// }
-
-// func multiplication(num1 int, num2 int) int {
-// 	return num1 * num2
-// }
-
-// func printToN(n int) {
-// 	for i := 0; i <= n; i++ {
-// 		fmt.Println(i)
-// 	}
-
-// 	for i := range n {
-// 		fmt.Println(i)
-// 	}
-// }
-
-// func sisipAngka(angka int, num []int) []int {
-// 	num = slices.Insert(num, 3, angka)
-// 	return num
-// }
 
 type education struct {
 	name  string
